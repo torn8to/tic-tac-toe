8b861d710545f92465d7d3e20108da6d563f496a
Nathan Rogers <nathanroger314@gmail.com>
2021-01-25 11:11:03 -0500

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -1,19 +1,28 @@
 import cv2 as cv
 import numpy as np
-import PIL as pil
 
 class TicTacToeBoard:
     def __init__(self,againstComputer:bool):
-        self.count = 1
+        self.turn_count = 1
         self.board = np.zeros((3,3), np.uint8)
         self.player1marker = 1
         self.player2marker = 2
         self.against_computer:bool = againstComputer
         self.player_is_first:bool = True
+        self.isPlayer1 = True
 
     def resetBoard(self):
         self.board = np.zeros((3,3), np.uint8)
 
+
+    def get_player_1_marker(self):
+        return self.player1marker
+
+    def get_player_2_marker(self):
+        return self.player2marker
+
+    def get_board(self):
+        return self.board
 
     '''
     checks either player conforms to a win condition
@@ -69,59 +78,18 @@ class TicTacToeBoard:
 
 
 
-    def playerPlays(self, isplayer1:bool,coordinateX, coordinateY):
+    def playerPlays(self, coordinateX, coordinateY):
 
-        if isplayer1:
+        if self.isPlayer1:
             playerMarker = self.player1marker
         else:
             playerMarker = self.player2marker
 
         self.board[coordinateY][coordinateX] = playerMarker
-
+        self.isPlayer1 =  not self.isPlayer1
 
     def printBoard(self):
         print(self.board)
-
-
-    def play_through_text_input(self):
-        IsPlaying = True
-        isPlayer1 = True
-        count == 0
-        while IsPlaying:
-            s = ''
-            if isPlayer1:
-                s = 'player 1'
-            else:
-                s = 'player 2'
-
-            print(self.board)
-            print("coordinates are 0,0 (top left) and coordinates are 2,2 bottom right")
-            x = int(input(s+ 'enter your x coordinate'))
-            y = int(input(s+ 'enter your y coordinate'))
-
-            if x >2 and y > 2 or self.coordinate_overlap(x,y):
-                input_is_valid = False
-                while not input_is_valid:
-                    print("please enterin a valid input 0-2 and in non filled coordinates")
-                    x = int(input(s+ 'enter your x coordinate'))
-                    y = int(input(s+ 'enter your y coordinate'))
-                    input_is_valid = x >2 and y > 2 or self.coordinate_overlap(x,y)
-
-            self.playerPlays(isPlayer1,x,y)
-            count = count+1;
-
-            if self.check_for_win() == 1 or self.check_for_win() == 2:
-                print("player "+ self.check_for_win()+ "wins")
-                IsPlaying = False
-            elif count == 8:
-                print("the game was a draw would you like to play again")
-
-
-            if not isPlaying:
-                i = str(input("would you like to play again Y/N"))
-                if i == "Y"or i == "y":
-                    self.resetBoard()
-                    isPlaying = True
 
 
 '''
@@ -155,14 +123,16 @@ def interact(event,x,y,flags,param):
     '''variables for figuring out which location the box is interacting   '''
     xx,yy = -1,-1
     in_region = False
-    #if event == cv.EVENT_LBUTTONDOWN:
-    if True:
+    if event == cv.EVENT_LBUTTONDOWN:
         for i in range(1,4):
             if left_pad + (i-1)*tic_tac_dimension +bar_width*(i-1) < x and x <= left_pad + (i* tic_tac_dimension)+ bar_width* (i-1):
-                xx = i
+                xx = i-1
             if left_pad + (i-1)*tic_tac_dimension +bar_width*(i-1) < y and y <= left_pad + (i* tic_tac_dimension)+ bar_width* (i-1):
-                yy = i
-    print(str(in_region) + ":("+str(xx)+","+str(yy)+")")
+                yy = i-1
+            tic_tac_toe_board.playerPlays(xx,yy)
+            tic_tac_toe_board.printBoard()
+            place_markers()
+    print(" yo")
 
 
 
@@ -175,7 +145,7 @@ End of interact function
 this function ismeant to switch the value of a boolean so that it is usable
 '''
 def switch(*args):
-    running = not running
+    cv.destroyAllWindows()
 
 '''
 function that enables the gui to reset the board
@@ -184,7 +154,42 @@ def reset_the_board(*args):
     tic_tac_toe_board.resetBoard()
     print("the succ")
 
-def
+'''
+this is to reset the board to the whit background and the tic tac toe board
+'''
+def replace_section(primary_section,new_addition, starting_x, starting_y):
+    primary_section[starting_x:starting_x+80,starting_y:starting_y+80] = new_addition
+    return primary_section
+'''
+writes pushes all of the relevant markers to the screen
+'''
+def place_markers():
+    board = tic_tac_toe_board.get_board()
+    player1 = tic_tac_toe_board.get_player_1_marker()
+    player2 = tic_tac_toe_board.get_player_2_marker()
+    for i in range(0,2):
+        for j in range(0,2):
+            if board[i][j] == player1:
+                replace_section(img,x_img,left_pad + (j*(tic_tac_dimension+bar_width)), top_pad+ j*(tic_tac_dimension*bar_width))
+            if board[i][j] == player2:
+                replace_section(img,o_img,left_pad + j*(tic_tac_dimension+bar_width), top_pad + (tic_tac_dimension*bar_width))
+
+'''
+resets the viewer to the tictac toe board with a white background
+'''
+def reset_the_viewer():
+    # resets img to an allwhite background
+    img = np.full((512,512,3),255,np.uint8)
+    # drawing the left vertical bar
+    cv.rectangle(img,(left_vert_init_x,left_vert_init_y),(left_vert_final_x,left_vert_final_y),bar_color,-1)
+    # drawing the right vertical bar
+    cv.rectangle(img,(right_vert_init_x,right_vert_init_y),(right_vert_final_x,right_vert_final_y), bar_color, -1)
+    # drawing the top horizontal bar
+    cv.rectangle(img,(top_hor_init_x,top_hor_init_y),(top_hor_final_x,top_hor_final_y), bar_color, -1)
+    # drawing the bottom horizontal bar
+    cv.rectangle(img,(bottom_hor_init_x,bottom_hor_init_y),(bottom_hor_final_x,bottom_hor_final_y),bar_color, -1)
+
+
 
 if __name__ == '__main__':
     '''start and end coordinates for the left vertical bar'''
@@ -213,7 +218,7 @@ if __name__ == '__main__':
 
 
     cv.namedWindow('image')
-    cv.createButton("Exit Game", switch,'image',cv.QT_PUSH_BUTTON,1) # when this button is hit it breaks the for loop
+    cv.createButton("Exit Game", switch,'image',cv.QT_PUSH_BUTTON,1) # when this button is hit it breaks the loop
     cv.createButton("Reset Board",reset_the_board,'image',cv.QT_PUSH_BUTTON,0) # resets the board
     cv.setMouseCallback('image', interact)
 
@@ -227,7 +232,8 @@ if __name__ == '__main__':
     # drawing the bottom horizontal bar
     cv.rectangle(img,(bottom_hor_init_x,bottom_hor_init_y),(bottom_hor_final_x,bottom_hor_final_y),bar_color, -1)
 
-    
+
+
 
     while running:
         cv.imshow('image',img)
